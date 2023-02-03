b884f0941927b1a2a52c46a7e59918e46cfb24b1
Felix Schoeller <felix.schoeller@protonmail.com>
2023-02-03 21:47:25 +0100

    player2

--- assets/scripts/level1/Player.gd
+++ assets/scripts/level1/Player.gd
@@ -1,16 +1,15 @@
 extends Node2D
 
+onready var anim: AnimationPlayer = $AnimationPlayer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta: float):
+	if Input.is_action_just_pressed("attack"):
+		print("attack")
+		anim.current_animation = "swipe"
+		anim.play()
+	if Input.is_action_pressed("attack"):
+		print("other attack")
