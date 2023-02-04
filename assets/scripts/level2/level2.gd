a1089849a737c6734004464252cf3d9bd1098252
Timmuh <github@timmuh.dev>
2023-02-04 03:09:07 +0100

    Slider Progressbar

--- assets/scripts/level2/level2.gd
+++ assets/scripts/level2/level2.gd
@@ -6,10 +6,21 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("attack"):
 		get_node("A-Button").scale = Vector2(3,3)
+		$AttackProgress.stop()
+		$SliderMaster.value += 1
 	if event.is_action_released("attack"):
+		$AttackProgress.start()
 		get_node("A-Button").scale = Vector2(4,4)
 
 	if event.is_action_pressed("dodge"):
 		get_node("B-Button").scale = Vector2(3,3)
+		$AttackProgress.stop()
+		$SliderMaster.value += 1
 	if event.is_action_released("dodge"):
+		$AttackProgress.start()
 		get_node("B-Button").scale = Vector2(4,4)
+
+
+func _on_AttackProgress_timeout():
+	if $SliderMaster.value >= 0:
+		$SliderMaster.value -= 1
