e2c1cff63805072ed1b1c3e05b554e4c912a3421
Felix Schoeller <felix.schoeller@protonmail.com>
2023-02-04 17:10:50 +0100

    jklasdjfkljsdalkfjlk

--- assets/scripts/level1/Foam.gd
+++ assets/scripts/level1/Foam.gd
@@ -16,7 +16,7 @@ func _physics_process(_delta: float):
 		return
 
 	if rng.randi_range(1, 200) == 1:
-		var micheal = bubble.instance().init(Vector2(rng.randi(), rng.randi()))
+		var micheal = bubble.instance().init(Vector2(rng.randi() - 2147483647.5, rng.randi() - 2147483647.5))
 		micheal.global_position = global_position
 		micheal.scale = scale
 		get_parent().add_child(micheal)
