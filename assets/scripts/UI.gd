90db3939d44b972bff7aa9bbf3c7a3da8ac86dc9
K-Win <Marc_klever@outlook.de>
2023-02-04 13:09:33 +0100

    Update UI.gd

--- assets/scripts/UI.gd
+++ assets/scripts/UI.gd
@@ -4,3 +4,6 @@ var heart_size: int = 64
 
 func on_player_life_changed(player_hearts: float):
 	$Hearts.rect_size.x = player_hearts * heart_size
+	
+func on_dashCD_changed(cd: float):
+	$DashCD.value += cd
