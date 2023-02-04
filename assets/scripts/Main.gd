fbd460b2774cd788a1398a425f3cb6649834016d
K-Win <Marc_klever@outlook.de>
2023-02-04 02:44:20 +0100

    bwushy bwushy

--- assets/scripts/Main.gd
+++ assets/scripts/Main.gd
@@ -1,16 +1,8 @@
 extends Node2D
 
+export(PackedScene) var menu_scene
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	var menu = menu_scene.instance()
+	add_child(menu)
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- assets/scripts/Menu.gd
+++ assets/scripts/Menu.gd
@@ -3,7 +3,13 @@ extends CanvasLayer
 onready var btnStart = $VBoxContainer/HContainer/PanelLinks/CenterContainer/VBoxContainer/Start
 onready var btnOptions = $VBoxContainer/HContainer/PanelLinks/CenterContainer/VBoxContainer/Start
 onready var btnQuit = $VBoxContainer/HContainer/PanelLinks/CenterContainer/VBoxContainer/Start
+var state = GameData.gamestate
 
 func _ready():
-	pass # Replace with function body.
+	if state == GameData.GameState.INGAME:
+		get_tree().paused = true
+		btnStart.text = "Resume"
 
+func _process(_delta):
+	if btnStart.get_focus_owner() == null:
+		btnStart.grab_focus()
