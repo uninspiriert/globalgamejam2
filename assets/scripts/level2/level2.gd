b4dc221180a28276ccf8b43c0e9178324477b705
K-Win <Marc_klever@outlook.de>
2023-02-05 12:46:02 +0100

    baka

--- assets/scripts/level2/level2.gd
+++ assets/scripts/level2/level2.gd
@@ -6,16 +6,23 @@ onready var slider = $QE/Slider
 onready var shaker = $Camera2D/ShakeCamera
 onready var gamewon = load("res://assets/scenes/GameEnd.tscn")
 onready var gameover = load("res://assets/scenes/GameOver.tscn")
-var rem_hitpoints = 30
+var rem_hitpoints = 2
 onready var lable_rem_hitpoints = $Camera2D/HBoxContainer/Value
 var velocity :float = 0.0
 var rndweight
+var countdown = 3
+export(PackedScene) var bloodspawn
 
 func _ready():
+	$AudioStreamPlayer.play()
+	get_tree().paused = true
 	lable_rem_hitpoints.text = str(rem_hitpoints)
 	rndweight = rand_range(-15, 15)
 	velocity += rndweight
 	pass
+	
+func showmessage(message):
+	$QE/Message.text = str(message)
 
 func _input(event):
 	var rndpower = rand_range(0,10)
@@ -51,4 +58,36 @@ func _process(delta):
 	if slider.value == 0 or slider.value == 100:
 		var _gameover = get_tree().change_scene_to(gameover)
 	if rem_hitpoints == 0:
-		var _gamewon = get_tree().change_scene_to(gamewon)
+		level_success()
+
+
+func level_success():
+	if $Tooth.visible == true:
+		$Tooth.hide()
+		$ToothRight.hide()
+		$Toof.show()
+		var spawnblood = bloodspawn.instance()
+		spawnblood.position = $Bloodspawn.position
+		spawnblood.scale = Vector2(10,10)
+		add_child(spawnblood)
+		$AnimationPlayerEnd.play("TakeOff")
+
+func _on_StartTimer_timeout():
+	if countdown > 0:
+		var text = str(countdown) + "..."
+		showmessage(text)
+		countdown -= 1
+	else:
+		$QE/Message.hide()
+		get_tree().paused = false
+		
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	$SceneChange.start()
+	$Toof.queue_free()
+	
+
+
+func _on_SceneChange_timeout():
+	var _gamewon = get_tree().change_scene_to(gamewon)
