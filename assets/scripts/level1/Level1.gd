6b26b4376d389e975cd1acb79c1d6d0da920e1bd
K-Win <Marc_klever@outlook.de>
2023-02-05 14:22:43 +0100

    yolo

--- assets/scripts/level1/Level1.gd
+++ assets/scripts/level1/Level1.gd
@@ -6,9 +6,6 @@ func _process(_delta):
 	if get_tree().get_nodes_in_group("teeth").size() == 0:
 		switch_scene()
 		
-func _input(event):
-	if event.is_action("ui_select"):
-		switch_scene()
 
 func switch_scene():
 	var level2 = level2_scene.instance()
